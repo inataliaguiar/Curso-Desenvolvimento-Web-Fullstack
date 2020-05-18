73bd82b425fc729281f851c706b24df6908a231c
Natali Rocha <natali.aguiar@icloud.com>
2020-05-17 22:10:11 -0300

    Create exContarCaracteres.js

---
+++ Introducao-JS/Exercicios/String/exContarCaracteres.js
@@ -0,0 +1,11 @@
+/** Exercício contar os caracteres
+ * A primeira coisa que você precisa fazer é declarar uma variável chamada "texto" atribuindo 
+ * como valor um texto,  que você preferir. Em seguida, usando a propriedade length, você precisará 
+ * imprimir no console o total de caracteres que a frase contém.
+ * 
+ */
+
+ var texto = "Este é um exemplo utilizando o 'length'!";
+ 
+ console.log(texto.length); //Retorna a quantidade de caracteres presentes na variável "texto"
+ 
