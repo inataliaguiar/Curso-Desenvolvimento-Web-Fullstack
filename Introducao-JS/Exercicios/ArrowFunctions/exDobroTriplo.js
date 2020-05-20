34c6e1678e97aa06d33765ce10f56fd44d678f0c
Natali Rocha <natali.aguiar@icloud.com>
2020-05-20 10:03:40 -0300

    Exercício dobro e triplo usando arrow function

---
+++ Introducao-JS/Exercicios/ArrowFunctions/exDobroTriplo.js
@@ -0,0 +1,14 @@
+function dobro(numero){
+    return numero * 2;
+}
+
+function triplo (numero){
+    return numero * 3;
+}
+
+function aplicar(numero, funcaoCallback){
+    return funcaoCallback(numero);
+
+}
+
+console.log(10, triplo);
