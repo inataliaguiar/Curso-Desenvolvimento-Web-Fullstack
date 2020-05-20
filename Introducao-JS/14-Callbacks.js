5baaf047f557be32c804c7e24e4626dbc62c5bc1
Natali Rocha <natali.aguiar@icloud.com>
2020-05-20 09:44:33 -0300

    Aula 14 - Callback
    Um callback é uma função que  recebe como parâmetro para outra função

---
+++ Introducao-JS/14-Callbacks.js
@@ -0,0 +1,39 @@
+/** Introdução a Javascript - Callbacks
+ * 
+ * Um callback é uma função que se passa como parâmetro para outra função
+ * 
+ * A função que recebe por parâmetro é a que se encarrega de executar quando é necessário.
+ * 
+ * Tipos de callback
+ *  Anônimo: A função passada como callback não tem nome, por esse motivo elas não podem ser chamadas
+ *  por seu nome, precisamos escrevê-la dentro da chamada da função  callback
+ * 
+ * setTimeout ( function (){
+ *  console.log("Hello World!");
+ * 
+ * } , 1000)
+ * 
+ */
+
+//Modo 1 (modo normal)
+setTimeout ( function (){
+    console.log("Hello World!");
+
+} , 1000)
+
+//Modo 2 (Com arrow function)
+
+let meuCallback = () => console.log("Hello World com callback e arrow function");
+setTimeout( meuCallback, 1000);
+
+
+//Outros exemplos
+function nomeCompleto(nome, sobrenome){
+    return nome + " " + sobrenome;
+
+}
+
+function saudar(nome, sobrenome, callback){
+    return "Olá " + callback(nome, sobrenome) + "!";
+
+}
