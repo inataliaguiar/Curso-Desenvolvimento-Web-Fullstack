b77337d302f135b8c11169b85b4ac24d7ab3ef34
Natali Rocha <natali.aguiar@icloud.com>
2020-05-26 01:35:40 -0300

    Update 13-ArrowFunction.js

--- Introducao-JS/13-ArrowFunction.js
+++ Introducao-JS/13-ArrowFunction.js
@@ -14,19 +14,17 @@
  * 
  *  Com um parâmetro (se parâmetros):
  *  var dobro = a => a * 2
- * 
  */
 
- //Exemplos
+//Exemplos
 
- let somar = (a, b) => a + b;
+let somar = (a, b) => a + b;
 
- //Exibindo o resultado da função
- console.log(somar(10, 20));
- 
+//Exibindo o resultado da função
+console.log(somar(10, 20));
 
- let elevarNum1aNum2 = (numero1, numero2) => Math.pow(numero1, numero2);
- console.log(elevarNum1aNum2(10, 2));
- 
+let elevarNum1aNum2 = (numero1, numero2) => Math.pow(numero1, numero2);
+console.log(elevarNum1aNum2(10, 2));
+
 let exibirMensagem = () => "Olá, estamos exibindo esta mensagem utilizando arrow functions!"
 console.log(exibirMensagem());
