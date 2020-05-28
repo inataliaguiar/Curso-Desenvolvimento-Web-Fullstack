5bc14de586139fcc89456239b24249e3e9a5c65a
Natali Rocha <natali.aguiar@icloud.com>
2020-05-27 22:30:15 -0300

    Update 16-MetodosDeArray.js

--- Introducao-JS/16-MetodosDeArray.js
+++ Introducao-JS/16-MetodosDeArray.js
@@ -8,9 +8,11 @@
  * .unshift(): adiciona os elementos passados por parâmetro no inicio do array 
  * .join(): junta os elementos de um array usando o separador que foi especificado e retorna uma string com os elementos unidos
  * .indexOf(): procura um elemento que recebeu como parâmetro e retorna seu respectivo índice
+ * .lastIndexOf(): procura o elemento que recebeu como parâmetro e retorna o índice, porém inicia a busca de trás para a frente
+ * 
+ * 
  * 
  * /
-
 
 /*Exemplos: */
 
@@ -31,8 +33,10 @@ console.log("Array posicoes após a remoção do primeiro elemento: " + posicoes
 var dispositivos = ["Notebook"];
 dispositivos.unshift("Smartphone"); //Método unshift para adicionar elementos no inicio do array
 
-var diasDaSemana = ["Segunda","Terça","Quarta","Quinta","Sexta"];
+var diasDaSemana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
 console.log(diasDaSemana.join(" - ")); //Método join junta os elementos do array separando eles por traço
 
 console.log(diasDaSemana.join("-feira ")); //Junta os elementos do array acrescentando "-feira"
 console.log(diasDaSemana.indexOf("Sexta")); //Retorna o índice do array correspondente ao parâmetro especificado
+console.log(diasDaSemana.lastIndexOf("Sexta")); //Começa a contagem de trás para frente e retorna o índice do array correspondente ao parâmetro especificado
+
