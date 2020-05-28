c124bbdb5ce3755f0711fd974302ee0527ad89a9
Natali Rocha <natali.aguiar@icloud.com>
2020-05-27 21:41:00 -0300

    Update 16-MetodosDeArray.js

---
+++ Introducao-JS/16-MetodosDeArray.js
@@ -0,0 +1,16 @@
+/** Introdução a Javascript - Métodos de arrays parte I
+ * 
+ * Os métodos em arrays permitem trabalhar com as informações presentes em um array de diversas formas
+ * 
+ * .push(): adiciona um ou mais elementos no final do array
+ * 
+ * 
+ */
+
+
+ //exemplos:
+ var cores = ["Azul", "Vermelho"];
+ console.log("Array cores antes da inserção de novos elementos: " + cores);
+ 
+ cores.push("Roxo","Preto"); //Método push para adicionar elementos ao array
+ console.log("Array cores após a inserção de novos elementos: " + cores);
